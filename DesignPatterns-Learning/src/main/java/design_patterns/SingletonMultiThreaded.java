294921974c6d13d1429b8bb814bd992e2002f8e6
Shivam Mishra <ip6.sam@gmail.com>
2025-01-16 13:37:13 +0530

    MultiThreaded Singleton DP Introduced with reflection check

--- DesignPatterns-Learning/src/main/java/design_patterns/SingletonMultiThreaded.java
+++ DesignPatterns-Learning/src/main/java/design_patterns/SingletonMultiThreaded.java
@@ -1,9 +1,13 @@
 package design_patterns;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class SingletonMultiThreaded {
     private static SingletonMultiThreaded instance = null;
 
     private SingletonMultiThreaded() {
+        if (instance != null) throw new RuntimeException("Object creation not allowed");
     }
 
 
@@ -19,11 +23,22 @@ public class SingletonMultiThreaded {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+            throws NoSuchMethodException, InvocationTargetException,
+            InstantiationException, IllegalAccessException {
         SingletonMultiThreaded ins1 = SingletonMultiThreaded.getInstance();
         SingletonMultiThreaded ins2 = SingletonMultiThreaded.getInstance();
 
         System.out.println(ins1.hashCode());
         System.out.println(ins2.hashCode());
+
+
+//        Using Reflection Approach
+        Class<SingletonMultiThreaded> clazz = SingletonMultiThreaded.class;
+        Constructor<SingletonMultiThreaded> declaredConstructor = clazz.getDeclaredConstructor();
+        declaredConstructor.setAccessible(true);
+
+        SingletonMultiThreaded o = (SingletonMultiThreaded) declaredConstructor.newInstance();
+        System.out.println(o.hashCode());
     }
 }
