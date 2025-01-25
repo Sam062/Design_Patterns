25637f0f1b2ee9f5b31234c314772f1aacc2ee38
Shivam Mishra <ip6.sam@gmail.com>
2025-01-25 18:26:58 +0530

    MultiThreaded Singleton DP Introduced with reflection check and Serialization.

--- DesignPatterns-Learning/src/main/java/design_patterns/SingletonMultiThreaded.java
+++ DesignPatterns-Learning/src/main/java/design_patterns/SingletonMultiThreaded.java
@@ -1,12 +1,14 @@
 package design_patterns;
 
+import java.io.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
-public class SingletonMultiThreaded {
+public class SingletonMultiThreaded implements Serializable {
     private static SingletonMultiThreaded instance = null;
 
     private SingletonMultiThreaded() {
+        /*Uncomment below code block to test to restrict object creation using Reflection API*/
         if (instance != null) throw new RuntimeException("Object creation not allowed");
     }
 
@@ -22,10 +24,15 @@ public class SingletonMultiThreaded {
         return instance;
     }
 
+    /*to fix serialization break.
+    whenever de-serialization happens, readResolve() is internally called.*/
+    protected Object readResolve() {
+        return instance;
+    }
 
     public static void main(String[] args)
             throws NoSuchMethodException, InvocationTargetException,
-            InstantiationException, IllegalAccessException {
+            InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
         SingletonMultiThreaded ins1 = SingletonMultiThreaded.getInstance();
         SingletonMultiThreaded ins2 = SingletonMultiThreaded.getInstance();
 
@@ -33,12 +40,34 @@ public class SingletonMultiThreaded {
         System.out.println(ins2.hashCode());
 
 
-//        Using Reflection Approach
+        /*Using Reflection Approach
+         * Comment out the constructor code block to test
+         * */
         Class<SingletonMultiThreaded> clazz = SingletonMultiThreaded.class;
         Constructor<SingletonMultiThreaded> declaredConstructor = clazz.getDeclaredConstructor();
         declaredConstructor.setAccessible(true);
 
         SingletonMultiThreaded o = declaredConstructor.newInstance();
         System.out.println(o.hashCode());
+
+
+        /*Serialization case breaks the pattern hence we need to add readResolve() impl because while de-serializing
+        internally it calls readResolve()*/
+
+        /*Without Serialization fix-- Comment readResolve()
+         * if we comment out the readResolve() impl then it will provide a new object which breaks the Singleton object creation rule
+         * */
+        SingletonMultiThreaded obj1 = SingletonMultiThreaded.getInstance();
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Object.txt"));
+        oos.writeObject(obj1);
+        oos.close();
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Object.txt"));
+        SingletonMultiThreaded obj2 = (SingletonMultiThreaded) ois.readObject();
+        ois.close();
+
+        System.out.println("--- Serialization Problem Objects ---");
+        System.out.println(obj1.hashCode());
+        System.out.println(obj2.hashCode());
     }
 }
